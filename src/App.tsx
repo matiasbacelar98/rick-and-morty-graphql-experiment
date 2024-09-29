ada45c48f98058d381f78c5c52995b9f6ede708b
Matias Bacelar <matiasbacelar@gmail.com>
2024-09-29 02:28:02 -0300

    Font setup and Header component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import { ApolloProvider } from '@apollo/client';
 import { ThemeProvider } from './context';
 import client from './config';
 
-import { Container } from './components';
+import { Container, Header } from './components';
 
 export default function App() {
   return (
     <ApolloProvider client={client}>
       <ThemeProvider>
+        <Header />
         <Container>
           <h1 className='text-3xl font-bold underline'>Working...</h1>
         </Container>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,15 @@
+import Container from './Container';
+import ThemeToggle from './ThemeToggle';
+
 export default function Header() {
-  return 'Header';
+  return (
+    <header className='py-3 shadow-md'>
+      <Container>
+        <div className='flex items-center justify-between'>
+          <h1 className='text-lg font-semibold text-slate-700'>Countries</h1>
+          <ThemeToggle />
+        </div>
+      </Container>
+    </header>
+  );
 }
