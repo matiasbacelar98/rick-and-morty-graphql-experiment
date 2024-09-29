665913cd656d155141780331c1d14c24a24cfef4
Matias Bacelar <matiasbacelar@gmail.com>
2024-09-29 02:10:35 -0300

    Container component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import { ApolloProvider } from '@apollo/client';
 import { ThemeProvider } from './context';
 import client from './config';
 
+import { Container } from './components';
+
 export default function App() {
   return (
     <ApolloProvider client={client}>
       <ThemeProvider>
-        <h1 className='text-3xl font-bold underline'>Working...</h1>
+        <Container>
+          <h1 className='text-3xl font-bold underline'>Working...</h1>
+        </Container>
       </ThemeProvider>
     </ApolloProvider>
   );
--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,3 +1,5 @@
-export default function Container() {
-  return 'Container';
+import React from 'react';
+
+export default function Container({ children }: React.PropsWithChildren) {
+  return <div className='container mx-auto p-4'>{children}</div>;
 }
